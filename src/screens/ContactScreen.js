35f37f7f6745f79518af2ad6625e59550e62ccd9
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-03-08 21:43:08 +0600

    Version 2.0

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { useState } from "react";
+import { useRef } from "react";
+
 // Styles
 import "../styles/contact-styles/contact_styles.css";
 
@@ -9,6 +12,41 @@ import arrow from "../assets/arrow_right.svg";
 import emailjs from "@emailjs/browser";
 
 const ContactScreen = () => {
+  const form = useRef();
+
+  // const [send, setSent] = useState("send");
+  // const [sendText, setSendText] = useState("Отправить");
+  const [fullName, setFullName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [city, setCity] = useState("");
+  const [mail, setMail] = useState("");
+
+  function handleClick() {
+    // setSent("send");
+    console.log("zhopa");
+  }
+
+  function sendEmail(e) {
+    e.preventDefault();
+    // setSent("sended");
+    setFullName("");
+    setPhoneNumber("");
+    setCity("");
+    setMail("");
+
+    emailjs
+      .sendForm(
+        "service_y27icff",
+        "template_evuaxxj",
+        e.target,
+        "bdkcSbsbst0EBRZfJ"
+      )
+      .then((res) => {
+        console.log("SUCCESS");
+      })
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className="contacts" id="contact-screen">
       <div className="contacts-container">
@@ -29,35 +67,55 @@ const ContactScreen = () => {
             </p>
           </section>
           {/* <img src={arrow} alt="arrow" className="arrow-svg"></img> */}
-          <form className="form" id="contacts_form">
+          <form
+            ref={form}
+            className="form"
+            id="contacts_form"
+            onSubmit={sendEmail}
+          >
             <input
               className="name-input"
               type="text"
+              name="name"
               placeholder="Имя"
+              value={fullName}
+              onChange={(event) => setFullName(event.target.value)}
               required={true}
             ></input>
             <input
               className="phone-input"
               type="tel"
+              name="phone"
+              minLength="10"
+              maxLength="11"
+              onChange={(event) => setPhoneNumber(event.target.value)}
+              value={phoneNumber}
               placeholder="Телефон"
               required={true}
             ></input>
             <input
               className="city-input"
               type="text"
+              name="city"
+              value={city}
+              onChange={(event) => setCity(event.target.value)}
               placeholder="Город"
               required={true}
             ></input>
             <input
               className="email-input"
+              name="email"
+              value={mail}
               type="email"
+              onChange={(event) => setMail(event.target.value)}
               placeholder="E-mail"
               required={true}
             ></input>
             <button
-              type="submit"
-              value="submit"
-              form="contacts_submit"
+              // type="submit"
+              value="Send"
+              onClick={handleClick}
+              // form="contacts_submit"
               className="submit-button"
             >
               ОТПРАВИТЬ ЗАЯВКУ
